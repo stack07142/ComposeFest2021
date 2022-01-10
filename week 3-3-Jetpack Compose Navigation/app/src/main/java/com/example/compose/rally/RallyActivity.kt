b5723572463708f70379750f265e4dc9d1f88c35
stack07142 <stack.07142@gmail.com>
2022-01-10 19:34:47 +0900

    compose navigation

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -19,16 +19,24 @@ package com.example.compose.rally
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
+import com.example.compose.rally.data.UserData
+import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
+import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
+import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
 
 /**
@@ -44,27 +52,76 @@ class RallyActivity : ComponentActivity() {
     }
 }
 
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate("${RallyScreen.Accounts.name}/$accountName")
+}
+
 @Composable
 fun RallyApp() {
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
-        var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Overview) }
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = RallyScreen.fromRoute(backstackEntry.value?.destination?.route)
+
         Scaffold(
             topBar = {
                 RallyTabRow(
                     allScreens = allScreens,
-                    onTabSelected = { screen -> currentScreen = screen },
+                    onTabSelected = { screen -> navController.navigate(screen.name) },
                     currentScreen = currentScreen
                 )
             }
         ) { innerPadding ->
-            Box(Modifier.padding(innerPadding)) {
-                currentScreen.content(
-                    onScreenChange = { screen ->
-                        currentScreen = RallyScreen.valueOf(screen)
-                    }
-                )
-            }
+            RallyNavHost(navController, Modifier.padding(innerPadding))
+        }
+    }
+}
+
+@Composable
+fun RallyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RallyScreen.Overview.name,
+        modifier = modifier
+    ) {
+        composable(RallyScreen.Overview.name) {
+            OverviewBody(
+                onClickSeeAllAccounts = { navController.navigate(RallyScreen.Accounts.name) },
+                onClickSeeAllBills = { navController.navigate(RallyScreen.Bills.name) },
+                onAccountClick = { name ->
+                    navigateToSingleAccount(navController, name)
+                }
+            )
+        }
+        composable(RallyScreen.Accounts.name) {
+            AccountsBody(UserData.accounts)
+        }
+        composable(RallyScreen.Bills.name) {
+            BillsBody(UserData.bills)
+        }
+
+        val accountsName = RallyScreen.Accounts.name
+        composable(
+            route = "$accountsName/{name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                }
+            ),
+            deepLinks = listOf(navDeepLink {
+                uriPattern = "rally://$accountsName/{name}"
+            })
+        ) { entry ->
+            val accountName = entry.arguments?.getString("name")
+            val account = UserData.getAccount(accountName)
+            SingleAccountBody(account = account)
         }
     }
 }
--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyScreen.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyScreen.kt
@@ -16,50 +16,39 @@
 
 package com.example.compose.rally
 
+import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AttachMoney
 import androidx.compose.material.icons.filled.MoneyOff
 import androidx.compose.material.icons.filled.PieChart
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.compose.rally.data.UserData
-import com.example.compose.rally.ui.accounts.AccountsBody
-import com.example.compose.rally.ui.bills.BillsBody
-import com.example.compose.rally.ui.overview.OverviewBody
 
 /**
  * Screen metadata for Rally.
  */
 enum class RallyScreen(
     val icon: ImageVector,
-    val body: @Composable ((String) -> Unit) -> Unit
 ) {
     Overview(
-        icon = Icons.Filled.PieChart,
-        body = { OverviewBody() }
+        icon = Icons.Filled.PieChart
     ),
     Accounts(
-        icon = Icons.Filled.AttachMoney,
-        body = { AccountsBody(UserData.accounts) }
+        icon = Icons.Filled.AttachMoney
     ),
     Bills(
-        icon = Icons.Filled.MoneyOff,
-        body = { BillsBody(UserData.bills) }
+        icon = Icons.Filled.MoneyOff
     );
 
-    @Composable
-    fun content(onScreenChange: (String) -> Unit) {
-        body(onScreenChange)
-    }
-
     companion object {
-        fun fromRoute(route: String?): RallyScreen =
-            when (route?.substringBefore("/")) {
+        fun fromRoute(route: String?): RallyScreen {
+            Log.d("toddtest", "route= $route")
+            return when (route?.substringBefore("/")) {
                 Accounts.name -> Accounts
                 Bills.name -> Bills
                 Overview.name -> Overview
                 null -> Overview
                 else -> throw IllegalArgumentException("Route $route is not recognized.")
             }
+        }
     }
 }
